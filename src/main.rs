4d8d8a2868a9339294631fb4ce216a17f581ccab
Teodoro <tedtke514@gmail.com>
2023-02-03 16:30:59 -0600

    main: created an html function to host logic for on clik

--- src/main.rs
+++ src/main.rs
@@ -19,4 +19,15 @@ fn app() -> Html {
             })
         })
     }
+
+    html! {
+        <div>
+            <button {onclick}>{"+1"}</button>
+            <p>{ state.value }</p>
+        </div>
+    }
+}
+
+fn main(){
+    yew::start_app::<App>():
 }
