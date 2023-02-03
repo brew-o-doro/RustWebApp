d2397c85269cdb8c1dc4cffd2c258fc3171952da
Teodoro <tedtke514@gmail.com>
2023-02-03 16:46:09 -0600

    main: refactored application index: added an html template to attach rust to styles: basic styling

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,30 @@ struct Model {
 
 #[function_component(App)]
 fn app() -> Html {
-    let state = use_state(|| Model{
-        value:0
+    let state = use_state(|| Model {
+        value: 0
     });
 
     let onclick = {
         let state = state.clone();
 
-        Callback::from(move |_|{
-            state.set(Model{
+        Callback::from(move |_| {
+            state.set(Model {
                 value: state.value + 1
             })
         })
-    }
+    };
 
     html! {
         <div>
-            <button {onclick}>{"+1"}</button>
+            <button {onclick}>
+                { "+1" }
+            </button>
             <p>{ state.value }</p>
         </div>
     }
 }
 
-fn main(){
-    yew::start_app::<App>():
+fn main() {
+    yew::start_app::<App>();
 }
